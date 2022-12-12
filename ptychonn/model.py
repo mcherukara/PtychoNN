09ce12221d50068bd20639d7251d34270c660d31
Daniel Ching <carterbox@users.noreply.github.com>
2022-12-12 12:19:17 -0600

    NEW: Add batchnormalization to ptychoNN

--- ptychonn/model.py
+++ ptychonn/model.py
@@ -5,18 +5,20 @@ import torch.nn as nn
 
 class ReconSmallPhaseModel(nn.Module):
 
-    def __init__(self, nconv: int = 16):
+    def __init__(self, nconv: int = 16, use_batch_norm=False):
         super(ReconSmallPhaseModel, self).__init__()
         self.nconv = nconv
+        self.use_batch_norm = use_batch_norm
 
         self.encoder = nn.Sequential(  # Appears sequential has similar functionality as TF avoiding need for separate model definition and activ
             *self.down_block(1, self.nconv),
             *self.down_block(self.nconv, self.nconv * 2),
             *self.down_block(self.nconv * 2, self.nconv * 4),
-            *self.down_block(self.nconv * 4, self.nconv * 8))
+            *self.down_block(self.nconv * 4, self.nconv * 8),
+        )
 
         # amplitude model
-        #self.decoder1 = nn.Sequential(
+        #self.decoder_amplitude = nn.Sequential(
         #    *self.up_block(self.nconv * 8, self.nconv * 8),
         #    *self.up_block(self.nconv * 8, self.nconv * 4),
         #    *self.up_block(self.nconv * 4, self.nconv * 2),
@@ -25,45 +27,61 @@ class ReconSmallPhaseModel(nn.Module):
         #)
 
         # phase model
-        self.decoder2 = nn.Sequential(
+        self.decoder_phase = nn.Sequential(
             *self.up_block(self.nconv * 8, self.nconv * 8),
             *self.up_block(self.nconv * 8, self.nconv * 4),
             *self.up_block(self.nconv * 4, self.nconv * 2),
             *self.up_block(self.nconv * 2, self.nconv * 1),
             nn.Conv2d(self.nconv * 1, 1, 3, stride=1, padding=(1, 1)),
-            nn.Tanh())
+            nn.BatchNorm2d(1) if self.use_batch_norm else torch.nn.Identity(),
+            nn.Tanh(),
+        )
 
     def down_block(self, filters_in, filters_out):
-        block = [
-            nn.Conv2d(in_channels=filters_in,
-                      out_channels=filters_out,
-                      kernel_size=3,
-                      stride=1,
-                      padding=(1, 1)),
+        return [
+            nn.Conv2d(
+                in_channels=filters_in,
+                out_channels=filters_out,
+                kernel_size=3,
+                stride=1,
+                padding=(1, 1),
+            ),
+            nn.BatchNorm2d(filters_out)
+            if self.use_batch_norm else torch.nn.Identity(),
             nn.ReLU(),
             nn.Conv2d(filters_out, filters_out, 3, stride=1, padding=(1, 1)),
+            nn.BatchNorm2d(filters_out)
+            if self.use_batch_norm else torch.nn.Identity(),
             nn.ReLU(),
             nn.MaxPool2d((2, 2))
         ]
-        return block
 
     def up_block(self, filters_in, filters_out):
-        block = [
-            nn.Conv2d(filters_in, filters_out, 3, stride=1, padding=(1, 1)),
+        return [
+            nn.Conv2d(
+                filters_in,
+                filters_out,
+                3,
+                stride=1,
+                padding=(1, 1),
+            ),
+            nn.BatchNorm2d(filters_out)
+            if self.use_batch_norm else torch.nn.Identity(),
             nn.ReLU(),
             nn.Conv2d(filters_out, filters_out, 3, stride=1, padding=(1, 1)),
+            nn.BatchNorm2d(filters_out)
+            if self.use_batch_norm else torch.nn.Identity(),
             nn.ReLU(),
             nn.Upsample(scale_factor=2, mode='bilinear')
         ]
-        return block
 
     def forward(self, x):
         with torch.cuda.amp.autocast():
             x1 = self.encoder(x)
-            #amp = self.decoder1(x1)
-            ph = self.decoder2(x1)
+            #amplitude = self.decoder_amplitude(x1)
+            phase = self.decoder_phase(x1)
 
             #Restore -pi to pi range
-            ph = ph * np.pi  #Using tanh activation (-1 to 1) for phase so multiply by pi
+            phase = phase * np.pi  #Using tanh activation (-1 to 1) for phase so multiply by pi
 
-        return ph
+        return phase
