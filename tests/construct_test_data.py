12e15f5c19274ba0d14ac6009811b98f89f811ad
Daniel Ching <carterbox@users.noreply.github.com>
2022-12-12 11:55:38 -0600

    Update training set with phase centered

--- tests/construct_test_data.py
+++ tests/construct_test_data.py
@@ -1,17 +1,23 @@
 import libimage
-import tifffile
 import numpy as np
+import tifffile
 import tike.ptycho
 import tike.ptycho.learn
 
+
 def test_construct_simulated_training_set(W=2048, N=500, S=64):
-    phase = libimage.load('coins', W)
+    phase = libimage.load('coins', W) - 0.5
     amplitude = 1 - libimage.load('earring', W)
-    fov = amplitude * np.exp(1j * phase * np.pi)
+    fov = (amplitude * np.exp(1j * phase * np.pi)).astype('complex64')
+    tifffile.imwrite('phase.tiff', np.angle(fov))
+    tifffile.imwrite('amplitude.tiff', np.abs(fov))
     assert np.abs(fov).max() <= 1.0
     assert np.abs(fov).min() >= 0.0
-    scan = np.random.uniform(1, W-S-1, (N, 2))
-    probe = (tike.ptycho.probe.gaussian(S)[None, None, None, ...]).astype('complex64')
+
+    scan = np.random.uniform(1, W - S - 1, (N, 2))
+
+    probe = (tike.ptycho.probe.gaussian(S)[None, None, None,
+                                           ...]).astype('complex64')
 
     patches = tike.ptycho.learn.extract_patches(
         psi=fov,
@@ -21,17 +27,21 @@ def test_construct_simulated_training_set(W=2048, N=500, S=64):
     print(patches.dtype, patches.shape)
     np.save('patches', patches)
 
-    diffraction = np.fft.ifftshift(tike.ptycho.simulate(
-        detector_shape=probe.shape[-1],
-        probe=probe,
-        scan=scan,
-        psi=fov,
-    ), axes=(-2, -1),).astype('float32')
+    diffraction = np.fft.ifftshift(
+        tike.ptycho.simulate(
+            detector_shape=probe.shape[-1],
+            probe=probe,
+            scan=scan,
+            psi=fov,
+        ),
+        axes=(-2, -1),
+    ).astype('float32')
     print(diffraction.dtype, diffraction.shape)
     np.save('diffraction', diffraction)
-    tifffile.imwrite('diffraction.tiff', np.log10(diffraction[N//2]))
+    tifffile.imwrite('diffraction.tiff', np.log10(diffraction[N // 2]))
 
     print(f'Training params = {np.prod(diffraction.shape)}')
+
 
 if __name__ == '__main__':
     test_construct_simulated_training_set()
