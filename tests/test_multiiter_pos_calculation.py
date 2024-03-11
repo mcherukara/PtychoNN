0314e148377f74e2eff0c702c5a886a99226eecb
Ming Du <ming.du101@gmail.com>
2024-03-11 14:48:10 -0500

    Additional comments in tester

--- tests/test_multiiter_pos_calculation.py
+++ tests/test_multiiter_pos_calculation.py
@@ -8,22 +8,32 @@ import numpy as np
 import ptychonn.pospred
 from ptychonn.pospred.configs import InferenceConfigDict
 from ptychonn.pospred.core import ProbePositionCorrectorChain
+from ptychonn.pospred.position_list import ProbePositionList
 
 
 def test_multiiter_pos_calculation():
     scan_idx = 235
 
-    config_dict = InferenceConfigDict(
+    configs = InferenceConfigDict(
         reconstruction_image_path=os.path.join('data', 'pospred', 'pred_test{}'.format(scan_idx), 'pred_phase.tiff'),
         random_seed=196,
         debug=False,
         probe_position_list=None,
-        central_crop=None
+        # To start with an initial position set, do the folliwing instead:
+        # probe_position_list=ProbePositionList(position_list=arr),
+        # where arr is a [N, 2] array of probe positions in pixel.
+        central_crop=None,
+        num_neighbors_collective=4,
     )
-    config_dict.load_from_toml(os.path.join('data', 'pospred', 'config_{}.toml'.format(scan_idx)))
-    print(config_dict)
+    # One can use different values for a config key at different iterations. To do this, create dict keys in the
+    # config object with a name of "<existing_config_ket_name>_multiiter". For example:
+    # configs.__dict__['method_multiiter'] = ["serial", "collective", "collective"]
+    # configs.__dict__['hybrid_registration_tols_multiiter'] = [[0.3, 0.15], [0.15, 0.1], [0.15, 0.1]]
+    # Alternatively, you may also put these multiiter keys in a json or toml and read them.
+    configs.load_from_toml(os.path.join('data', 'pospred', 'config_{}.toml'.format(scan_idx)))
+    print(configs)
 
-    corrector_chain = ProbePositionCorrectorChain(config_dict)
+    corrector_chain = ProbePositionCorrectorChain(configs)
     corrector_chain.verbose = False
     corrector_chain.build()
     corrector_chain.run()
